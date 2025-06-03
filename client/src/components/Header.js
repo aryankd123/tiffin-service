fb95bd377328d50bf11456b1d36d79c2286001fe
ARYAN <169179921+aryankd123@users.noreply.github.com>
2025-06-03 19:05:51 +0530

    contact page make and cart added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -36,12 +36,16 @@ function Header() {
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
             <Nav.Link as={Link} to="/subscriptions">Subscriptions</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+
           </Nav>
           
           <Nav className="align-items-center">
             {/* Cart Icon */}
             <Nav.Link as={Link} to="/cart" className="position-relative me-3">
-              <span style={{ fontSize: '20px' }}>🛒</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+</svg>
               {getCartItemsCount() > 0 && (
                 <Badge 
                   bg="danger" 
